ed26155d908d9cf85002726553f3471e8e0a1b3e
gabrieljablonski <gabriel.g.jablonski@gmail.com>
2022-02-15 21:42:11 -0300

    separate method from url in http logging

--- src/http/middlewares/logRequests.ts
+++ src/http/middlewares/logRequests.ts
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import { HttpCsvLogger } from 'logging';
 
 const logRequests = morgan(
-  ':date[iso],:remote-addr,:method :url,:status,:res[Remote-Calculator-Calculation-Id],:response-time',
+  ':date[iso],:remote-addr,:method,:url,:status,:res[Remote-Calculator-Calculation-Id],:response-time',
   {
     stream: {
       write: message => HttpCsvLogger.log({ level: 'info', message }),
